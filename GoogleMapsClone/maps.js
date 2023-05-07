5cc2d819f6668c750af0094322764b39c87fd340
bbram8 <130707789+bbram8@users.noreply.github.com>
2023-05-06 18:02:59 -0700

    Add files via upload

--- GoogleMapsClone/maps.js
+++ GoogleMapsClone/maps.js
@@ -1,1 +1,37 @@
+const MAPBOX_ACCESS_TOKEN =
+ "<Your_Mapbox_access_token>" ; 
 
+navigator.geolocation.getCurrentPosition(good_location, err_location, {
+  enableHighAccuracy: true
+})
+
+// start with setting up the map and the center position of your map
+// setup
+function map_setup(center_location) {
+  const map = new mapboxgl.Map({
+    accessToken: MAPBOX_ACCESS_TOKEN,
+    container: "map",
+    style: "mapbox://styles/mapbox/streets-v11",
+    center: center_location,
+    zoom: 35
+  })
+   // 
+  const dir_control = new MapboxDirections({
+    accessToken: MAPBOX_ACCESS_TOKEN
+  })
+  map.addControl(dir_control, "top-right");
+}
+
+  const navi_control = new mapboxgl.NavigationControl()
+  map.addControl(navi_control);
+ 
+
+// Mapbox API takes long-lat instead of lat-long like Google Maps :( 
+function good_location(position) {
+  map_setup([position.coords.longitude, position.coords.latitude]);
+}
+
+// where do I need to go to if there is an error in the location - default coords
+function err_location() {
+  map_setup([-2.24, 53.48])
+}
